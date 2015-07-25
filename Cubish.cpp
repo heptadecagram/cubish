987bb7dcfad3b59a33d56c9a6207367d078562ac
Liam Echlin <wechlin@umbctraining.com>
2015-07-25 04:55:25 -0400

    Who names args to main() like that?

--- Cubish.cpp
+++ Cubish.cpp
@@ -10,7 +10,7 @@
 // Description: This file contains the main() function for the program
 // _Liam's_Cubish_.  It creates a new Cube object and six Color objects,
 // stored in an array.
-int main(int Argument_Count, char** Arguments) {
+int main(int argc, char* argv[]) {
 
 	// Declare a default cube size
 	auto Width=3, Height=3, Depth=3;
@@ -28,7 +28,7 @@ int main(int Argument_Count, char** Arguments) {
 	auto Alpha=Cube(Width, Height, Depth, Side_Colors[0], Side_Colors[1], Side_Colors[2],
 			Side_Colors[3], Side_Colors[4], Side_Colors[5]);
 
-	Initialize_Window(Argument_Count, Arguments, Alpha, Side_Colors);
+	Initialize_Window(argc, argv, Alpha, Side_Colors);
 
 	glutMainLoop();
 
